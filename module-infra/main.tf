f21b6dc544b7824276973c15dd9eeb94593bba9d
ashokv <ashok3akr@gmail.com>
2025-04-29 17:19:17 +0530

    ansible

--- module-infra/main.tf
+++ module-infra/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
 # terraform {
 #   required_providers {
 #     aws = {
@@ -21,6 +29,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
+  discription       = "ssh"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_all_ipv4" {
@@ -29,6 +38,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_all_ipv4" {
   from_port         = var.port
   ip_protocol       = "tcp"
   to_port           = var.port
+  discription       = var.name
+}
+
+resource "aws_vpc_security_group_egress_rule" "egress_allow_all" {
+  security_group_id = aws_security_group.tool.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
 
 resource "aws_instance" "tool" {
